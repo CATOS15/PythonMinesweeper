718abeaa1d9416aef03897e63cfc2e11143debdc
Bircck <s195458@student.dtu.dk>
2021-03-05 13:03:30 +0100

    lavet rooms funktionalitet start ting

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
       <div>Besked fra backend (socket): {{socketMsg}}</div>
+      <div>
+        <input v-model="roomname" type="text" placeholder="ex. 53436" :disabled="socketestablishing"/>
+        <button @click="socketcreate(roomname)" :disabled="socketestablishing" >opret</button>
+        <button @click="socketjoin(roomname)" :disabled="socketestablishing" >join</button>
+      </div>
   </div>
 </template>
 
@@ -14,13 +19,34 @@ import SocketResponse from '@/models/socketResponse';
   },
 })
 export default class Home extends Vue {
-  socket: Socket = io('ws://' + location.hostname + ':5005');
+  socket!: Socket;
+  roomname = '459';
+
+  socketestablishing = false;
 
   socketMsg = '';
 
+  socketcreate(roomname: string){
+    this.socket.emit("createroom", JSON.stringify({
+      "roomname": roomname
+    }))
+  }
+
+  socketjoin(roomname: string){
+    this.socket.emit("joinroom", JSON.stringify({
+      "roomname": roomname
+    }))
+  }
+
   mounted(){
-    this.socket.on('emitTest', (resp: SocketResponse) => {
-      this.socketMsg = resp.data;
+    this.socketestablishing = true;
+    this.socket = io('ws://' + location.hostname + ':5005');
+    this.socket.on('connect', () => {
+      //debugger // eslint-disable-line
+      this.socketestablishing = false;
+      this.socket.on('emitTest', (resp: SocketResponse) => {
+        this.socketMsg = resp.data;
+      });
     });
   }
 }
