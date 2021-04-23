b8ba3326d6adef27f44b1bd03c65b08929765464
s184140 <s184140@student.dtu.dk>
2021-04-23 12:27:00 +0200

    database test

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,7 @@ class Database:
             database="minesweeper"
         )
         ## Global cursor
-        cur = self.con.cursor()
+        self.cur = self.con.cursor()
         print("Connection established")
 
     def get_highscores(self):
@@ -19,14 +19,14 @@ class Database:
 
     def insert_highscore(self,width,height,timer,numberOfPlayers,usernames):
         ## Creat user if no name exist
-        sql = "INSERT INTO highscores (width,height,timer,numberOfPlayers,usernames) VALUES (%d, %d, %d, %d, %s)"
+        sql = "INSERT INTO Highscore (width,height,timer,numberOfPlayers,usernames) VALUES (%s, %s, %s, %s, %s)"
         val = (width,height,timer,numberOfPlayers,usernames)
-        ##val = (24, 24, 200, 4, 'oliver, frost, tobi, soeren')
         self.cur.execute(sql, val)
 
         self.con.commit()
 
 
 
-
+database = Database()
+database.insert_highscore(24, 24, 200, 4, 'oliver, frost, tobi, soeren')
 
